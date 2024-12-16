078b783dd28278b308fea6de3137f235d6bbed40
Dana Ilnytska <volodere3@gmail.com>
2024-12-16 15:58:23 +0100

    homework_27

---
+++ src/lesson_27/persons/Person.java
@@ -0,0 +1,149 @@
+package lesson_27.persons;
+
+public class Person {
+    private String email;
+    private String password;
+
+
+    public Person(String email, String password) {
+        setEmail(email);
+        setPassword(password);
+    }
+
+
+       public String getEmail() {
+       return email;
+    }
+
+
+    public void setEmail(String email) {
+        if (isEmailValid(email))
+            this.email = email;
+    }
+     /*
+    vasya@gmail.com
+    @asyagmail.com
+    vasya@gmail.com
+    vasya@gmail.net.de
+    vasy12112a@gmail.net.de
+    vasya@gmail.net.de
+     */
+
+    /*
+    1. Должна присутствовать @ и только одна (done)
+    2. Точка после собаки
+    3. После последней точки должно быть минимум 2 символа
+    4. Алфавит, цифры, '_', '-', '.', '@'
+    5. До собаки должен быть хотя бы 1 символ
+    5. Первый символ должен быть буквой
+     */
+
+    private boolean isEmailValid(String email) {
+        // 1. Должна присутствовать @
+        int indexAt = email.indexOf('@');
+        if (indexAt == -1 || indexAt != email.lastIndexOf('@')) return false;
+
+        //2. Точка после собаки
+        if (email.indexOf('.', indexAt) == -1) return false;
+
+        //3.После последней точки должно быть минимум 2 символа
+        if (email.lastIndexOf('.') >= email.length() - 2) return false;
+
+        for (int i = 0; i < email.length(); i++) {
+            char ch = email.charAt(i);
+            if (!(Character.isAlphabetic(ch)
+                    || Character.isDigit(ch)
+                    || ch == '-'
+                    || ch == '_'
+                    || ch == '.'
+                    || ch == '@')) { //символ не  подходит
+                return false;
+            }
+
+        }
+        //5. До собаки должен быть хотя бы 1 символ
+        if (indexAt == 0) return false;
+
+        if (!Character.isAlphabetic(email.charAt(0))) return false;
+
+        return true;
+    }
+    //
+    /*
+    Требования к паролю
+    1. Длина >= 8
+    2. Должна быть мин 1 маленькая буква
+    3. Должна быть мин 1 большая буква
+    4. Должна быть мин 1 цифра
+    4. Должен быть мин 1 спец.символ "!%$@&*()[]"
+     */
+    //4 переменный типа boolean
+    //boolean isDigit = true;
+    //boolean isLowerCase = false;
+    // Запускаю цикл
+    // И после цикла во всех 4-х должено быть true. - тогда вернуть true
+    //  Иначе пароль проверку не прошел - вернуть false
+
+    boolean isDigit = false;
+    boolean isLowerCase = false;
+    boolean isUpperCase = false;
+    boolean isSpecSymbol = false;
+
+
+
+    public  boolean isPasswordValid(String password) {
+        if (password == null || password.length() < 8) {
+            return false;
+        }
+        String specChars = "!%$@&*()[]";
+
+
+
+        for (int i = 0; i < password.length(); i ++) {
+            char ch1 = password.charAt(i);
+
+
+            if (Character.isDigit(ch1)) {
+                isDigit = true;
+            }
+            else if (Character.isLowerCase(ch1)) {
+                isLowerCase = true;
+            }
+            else if (Character.isUpperCase(ch1)) {
+                isUpperCase = true;
+            }
+            else if (specChars.contains(String.valueOf(ch1))) {
+                isSpecSymbol = true;
+            }
+
+
+
+        }
+        return isDigit && isLowerCase && isUpperCase && isSpecSymbol;
+
+    }
+
+
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        if (isPasswordValid(password))
+
+        this.password = password;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
